f11f2b39a4fe96d009678ac244e68c0ed055d94e
arubinofaux <arubinofaux@gmail.com>
2021-08-04 18:01:23 -0600

    save hourly archive as json

--- src/jobs/twitter_post.cr
+++ src/jobs/twitter_post.cr
@@ -12,7 +12,7 @@ class TwitterPostJob < Mosquito::PeriodicJob
       REDIS.set("hourlysum_of_forks", sum_of_forks.to_s)
     else
       coin_lines : String = ""
-      archive : String = ""
+      archive = [] of NamedTuple(symbol: String, price: String, trend: String, change1h: String)
 
       coins.each do |coin|
         coin_symbol = coin["symbol"]
@@ -26,7 +26,12 @@ class TwitterPostJob < Mosquito::PeriodicJob
         sum_of_forks = (sum_of_forks + coin_price)
         coin_lines = coin_lines + new_line
 
-        archive = archive + ":#{coin_symbol},#{coin_price_formatted},#{coin_trend},#{coin_percent_change1h_rounded}%"
+        archive << {
+          symbol: coin_symbol,
+          price: coin_price_formatted,
+          trend: coin_trend,
+          change1h: coin_percent_change1h_rounded
+        } 
       end
 
       last_hourly_price = REDIS.get("hourlysum_of_forks").to_s.to_f
@@ -36,7 +41,7 @@ class TwitterPostJob < Mosquito::PeriodicJob
       fork_lines = "Current Bitcoin Price\nAll Forks = #{BVB::Services::Money.new(sum_of_forks)} #{trend(last_hour_diff.to_s)} #{last_hour_diff}%\n--\n" + coin_lines
 
       REDIS.set("hourlysum_of_forks", sum_of_forks.to_s)
-      REDIS.set("hourly_archive", archive)
+      REDIS.set("hourly_archive", archive.to_json)
       REDIS.set("last_tweet", fork_lines)
 
       TWITTERC.update(fork_lines) if ENV["APP_ENV"] == "production"
